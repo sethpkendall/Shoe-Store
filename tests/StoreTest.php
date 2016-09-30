aa4101d9044b6b5fbedc9f52e26b4dbb2cd48f44
Seth Kendall <sethpkendall@gmail.com>
2016-09-30 11:09:42 -0700

    add test 14

--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -90,6 +90,27 @@
             $this->assertEquals([$test_store, $test_store2], $result);
         }
 
+        function test_delete()
+            {
+              //Arrange
+              $name = "Foot Locker";
+              $phone = "888-888-8888";
+              $address = "123 Way Ave. Portland, OR 97204";
+              $test_store = new Store($name, $phone, $address);
+              $test_store->save();
+
+              $name2 = "Getcha Shoes Heah!";
+              $phone2 = "888-888-8887";
+              $address2 = "124 Way Ave. Portland, OR 97204";
+              $test_store2 = new Store($name2, $phone2, $address2);
+              $test_store2->save();
+              //Act
+              $test_store->delete();
+              $result = Store::getAll();
+              //Assert
+              $this->assertEquals([$test_store2], $result);
+            }
+
         function test_storeFind()
         {
             //Arrange
